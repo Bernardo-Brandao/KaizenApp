7fabc5635c74543d3d43ee3de7563907a5e0c3dc
Bernardo Brandão <bernardo.brandao11@gmail.com>
2026-08-05 23:14:09 -0300

    Initial Commit

---
+++ KaizenApp/KaizenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaizenAppApp.swift
+//  KaizenApp
+//
+//  Created by Bernardo Brandão on 05/08/26.
+//
+
+import SwiftUI
+
+@main
+struct KaizenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
